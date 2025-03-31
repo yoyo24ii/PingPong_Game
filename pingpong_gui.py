abc97eca3fa80c168d46a6738688d1b1a9dc34f3
yousf <yousefalsalti2411@outlook.com>
2025-03-31 15:12:33 +0300

    main pingpong function

---
+++ pingpong_gui.py
@@ -0,0 +1,28 @@
+"""
+-----------------------------------------------------
+Author: Yousf Al Salti
+Date: 31/03/2025
+Time: 2:11 PM
+Version: 1.0
+-----------------------------------------------------
+
+Filename: pingpong_gui
+
+-----------------------------------------------------
+Description: [Describe the purpose of the script]
+-----------------------------------------------------
+
+Inputs: [Describe inputs]
+Outputs: [Describe outputs]
+-----------------------------------------------------
+
+Contact Information:
+📧 Outlook: [yousefalsalti2411@outlook.com]
+📧 Gmail: [yousefalsalti024@gmail.com]
+🔗 GitHub: [https://github.com/yoyo24ii]
+🔗 LinkedIn: [https://www.linkedin.com/in/yousfsalti24ii/]
+-----------------------------------------------------
+
+Copyright (c) 2025 Yousf Al Salti. All rights reserved.
+-----------------------------------------------------
+"""
