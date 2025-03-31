6cdfe1e4057c69f412417df19894e0c86268abea
yousf <yousefalsalti2411@outlook.com>
2025-03-31 18:03:06 +0300

    working pingpong ball

--- pingpong_classes.py
+++ pingpong_classes.py
@@ -27,7 +27,7 @@ Copyright (c) 2025 Yousf Al Salti. All rights reserved.
 -----------------------------------------------------
 """
 
-import matplotlib.pyplot as plt
+import pygame
 class my_Object:
     object_count=0
     def __init__(self,color,filled):
@@ -46,19 +46,28 @@ class my_Circle(my_Object):
         self.radius=radius
         self.x_pos = 0
         self.y_pos = 0
+        self.vx = 0
+        self.vy = 0
+        pass
     #Seting the position of the center
     def position(self,x_pos=0,y_pos=0):
         self.x_pos=x_pos
         self.y_pos=y_pos
+    #setting the velocity of the center
+    def velocity(self,vx=0,vy=0):
+        self.vx=vx
+        self.vy=vy
 
     #moves the ball a given amount
-    def move(self,dx,dy):
-        self.x_pos+=dx
-        self.y_pos+=dy
+    def move(self,dt):
+        self.x_pos+=self.vx*dt
+        self.y_pos+=self.vy*dt
 
     #Coordinates of the ball
     def get_position(self):
         return self.x_pos,self.y_pos
+    def get_velocity(self):
+        return self.vx,self.vy
 
     #getter functions for attributes
     def get_radius(self):
@@ -73,9 +82,50 @@ class my_Circle(my_Object):
         print(f"Circle #{self.object_id} is {self.color} and {'filled' if self.filled else 'not filled' } with radius {self.radius}")
     pass
 
-def main():
-    circle=my_Circle(color="cyan",filled= True,radius=2)
 
+
+
+def main():
+    # Initialize Pygame
+    pygame.init()
+
+    # Set up display
+    WIDTH, HEIGHT = 500, 200
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+
+    #circle definition
+    circle = my_Circle("red",True,25)
+    circle.position(100,150)
+    circle.velocity(10,10)
+    running = True
+    color=circle.get_color()
+    x,y=circle.get_position()
+    radius=circle.get_radius()
+    while running:
+        screen.fill((0, 0, 0))  # Clear screen with black
+        pygame.draw.circle(screen, color, (x, y), radius)  # Draw circle
+        pygame.display.flip()  # Update display
+
+        # Move the circle
+        circle.move(1)
+        x,y=circle.get_position()
+        vx,vy=circle.get_velocity()
+        if x > WIDTH - radius or x < radius:
+            vx=-vx
+            circle.velocity(vx,vy)
+        if y > HEIGHT - radius or y < radius:
+            vy=-vy
+            circle.velocity(vx,vy)
+
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        clock.tick(30)  # Limit FPS
+
+    pygame.quit()
 
 
     pass
