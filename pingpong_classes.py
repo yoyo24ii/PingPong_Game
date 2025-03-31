9e5d670b4f792f98e2d56c9ea59da3d52fc9c605
yousf <yousefalsalti2411@outlook.com>
2025-03-31 16:50:23 +0300

    updated classes module

--- pingpong_classes.py
+++ pingpong_classes.py
@@ -27,20 +27,57 @@ Copyright (c) 2025 Yousf Al Salti. All rights reserved.
 -----------------------------------------------------
 """
 
-class Object:
-
+import matplotlib.pyplot as plt
+class my_Object:
+    object_count=0
     def __init__(self,color,filled):
+        my_Object.object_count += 1
+        self.object_id = my_Object.object_count
         self.color=color
         self.filled=filled
-        pass
 
-class Circle(Object):
+        pass
+    def describe(self):
+        print(f"Object #{self.object_id} is {self.color} and {'filled' if self.filled else 'not filled' }")
+
+class my_Circle(my_Object):
     def __init__(self,color, filled, radius):
         super().__init__(color,filled)
         self.radius=radius
+        self.x_pos = 0
+        self.y_pos = 0
+    #Seting the position of the center
+    def position(self,x_pos=0,y_pos=0):
+        self.x_pos=x_pos
+        self.y_pos=y_pos
+
+    #moves the ball a given amount
+    def move(self,dx,dy):
+        self.x_pos+=dx
+        self.y_pos+=dy
+
+    #Coordinates of the ball
+    def get_position(self):
+        return self.x_pos,self.y_pos
+
+    #getter functions for attributes
+    def get_radius(self):
+        return self.radius
+    def get_color(self):
+        return self.color
+    def get_filled(self):
+        return self.filled
+
+
+    def describe(self):
+        print(f"Circle #{self.object_id} is {self.color} and {'filled' if self.filled else 'not filled' } with radius {self.radius}")
     pass
 
 def main():
+    circle=my_Circle(color="cyan",filled= True,radius=2)
+
+
+
     pass
 
 if __name__ == "__main__":
